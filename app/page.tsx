defb3c777ffbd8e19217097a91fee3974f3ad586
Aiden052026 <nvt080481@gmail.com>
2026-05-18 20:24:00 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+export default function HomePage() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      background: "#020617",
+      color: "white",
+      padding: "60px",
+      fontFamily: "Arial"
+    }}>
+      <h1 style={{
+        fontSize: "72px",
+        color: "#3b82f6",
+        fontWeight: "900"
+      }}>
+        AMICO
+      </h1>
+
+      <p>Connecting Industrial Flow</p>
+
+      <h2 style={{
+        fontSize: "48px",
+        marginTop: "40px"
+      }}>
+        Website AMICO Việt Nam
+      </h2>
+    </main>
+  )
+}
